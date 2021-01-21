c2882401e1a10db46fad6ef0ff9afe8278214b08
khjoanna <77780938+khjoanna@users.noreply.github.com>
2021-01-21 15:53:28 +0100

    Add files via upload

---
+++ jar_vial_base.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="jar_vial_base" tilewidth="32" tileheight="32" tilecount="54" columns="6">
+ <image source="vial-jar-distractedmosfet/jar_vial_base.png" width="200" height="300"/>
+ <wangsets>
+  <wangset name="New Wang Set" tile="-1">
+   <wangedgecolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#ff7700" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00e9ff" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#ff00d8" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#ffff00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#a000ff" tile="-1" probability="1"/>
+   <wangcornercolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcornercolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangcornercolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangcornercolor name="" color="#ff7700" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>
