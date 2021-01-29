28de70ba5773f5889201187eaac6d6a23eb641cc
khjoanna <77780938+khjoanna@users.noreply.github.com>
2021-01-29 15:23:33 +0100

    Delete Font Pixels 01.tsx

--- Font Pixels 01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.0" name="Font Pixels 01" tilewidth="32" tileheight="32" tilecount="10" columns="5">
- <image source="Font Pixels 01.png" width="172" height="86"/>
-</tileset>
