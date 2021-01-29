196a8fa595c903605d17293f15cf2cc2698aadf0
khjoanna <77780938+khjoanna@users.noreply.github.com>
2021-01-29 15:23:44 +0100

    Delete _pixel_art_food__32x32_vegetables___free_download_by_anarchisedlute_dd0fukl.tsx

--- _pixel_art_food__32x32_vegetables___free_download_by_anarchisedlute_dd0fukl.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.0" name="_pixel_art_food__32x32_vegetables___free_download_by_anarchisedlute_dd0fukl" tilewidth="32" tileheight="32" tilecount="10" columns="5">
- <image source="_pixel_art_food__32x32_vegetables___free_download_by_anarchisedlute_dd0fukl.png" width="160" height="64"/>
-</tileset>
